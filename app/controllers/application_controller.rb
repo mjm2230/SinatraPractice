3032e4e2ad5214f4093bfb4a139cef411d4b1c52
Megan McGregor <mjm2230@gmail.com>
2018-07-17 20:36:44 +0000

    add sinatra practice

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+require './config/environment'
+require './app/models/model'
+
+class ApplicationController < Sinatra::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+    return erb :index
+  end
+  
+end
---
+++ app/models/model.rb
@@ -0,0 +1,9 @@
+def odd_or_even(phrase)
+   length = phrase.length
+   if length % 2 == 0
+       return "Your sentence contains an even number of characters."
+   else
+       return "Your sentence contains an odd number of characters"
+   end
+end
+
